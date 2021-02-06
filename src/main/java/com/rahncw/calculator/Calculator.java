794207d2e37c6823fe7def03740fcf8d0522f015
Chris Rahn <c.w.rahn@gmail.com>
2021-02-06 14:48:12 -0700

    Reformatting Calculator.java

--- src/main/java/com/rahncw/calculator/Calculator.java
+++ src/main/java/com/rahncw/calculator/Calculator.java
@@ -5,9 +5,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class Calculator {
 
-    int sum(int a, int b) {
-        return a + b;
-    }
+  int sum(int a, int b) {
+    return a + b;
+  }
 
 
 }
