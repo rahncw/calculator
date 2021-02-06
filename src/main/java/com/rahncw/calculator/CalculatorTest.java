077e3e230b318929ed289cdcf2b530d5c5e887db
Chris Rahn <c.w.rahn@gmail.com>
2021-02-06 11:56:28 -0700

    More messing around with build.gradle and moving test to proper location

--- src/main/java/com/rahncw/calculator/CalculatorTest.java
+++
@@ -1,4 +0,0 @@
-package com.rahncw.calculator;
-
-public class CalculatorTest {
-}
---
+++ src/test/java/com/rahncw/calculator/CalculatorTest.java
@@ -0,0 +1,17 @@
+package com.rahncw.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class CalculatorTest {
+
+    @Test
+    public void testSum() {
+        Calculator calculator = new Calculator();
+        Assertions.assertEquals(calculator.sum(5, 3), 8);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
