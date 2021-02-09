27ccfb3af8675ea4f97f5ec3ed8d33cca725fcd1
Chris Rahn <c.w.rahn@gmail.com>
2021-02-08 20:28:21 -0700

    Revert "Turn on hazelcast caching"
    This reverts commit fdd4eed86fd3f7bd0f23189693eae1d302c0e14c.

--- src/main/java/com/rahncw/calculator/Calculator.java
+++ src/main/java/com/rahncw/calculator/Calculator.java
@@ -1,7 +1,6 @@
 package com.rahncw.calculator;
 
 import org.springframework.stereotype.Service;
-import org.springframework.cache.annotation.Cacheable;
 
 /**
  * this is a javadoc.
@@ -9,25 +8,11 @@ import org.springframework.cache.annotation.Cacheable;
 @Service
 public class Calculator {
 
-  @Cacheable("sum")
   int sum(int a, int b) {
-    // sleep three seconds for testing purposes
-    try {
-      Thread.sleep(3000);
-    } catch (InterruptedException e) {
-      throw new RuntimeException(e);
-    }
     return a + b;
   }
 
-  @Cacheable("diff")
   int diff(int a, int b) {
-    // sleep three seconds for testing purposes
-    try {
-      Thread.sleep(3000);
-    } catch (InterruptedException e) {
-      throw new RuntimeException(e);
-    }
     return a - b;
   }
 
