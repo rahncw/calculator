fdd4eed86fd3f7bd0f23189693eae1d302c0e14c
Chris Rahn <c.w.rahn@gmail.com>
2021-02-08 20:02:08 -0700

    Turn on hazelcast caching

--- src/main/java/com/rahncw/calculator/Calculator.java
+++ src/main/java/com/rahncw/calculator/Calculator.java
@@ -1,6 +1,7 @@
 package com.rahncw.calculator;
 
 import org.springframework.stereotype.Service;
+import org.springframework.cache.annotation.Cacheable;
 
 /**
  * this is a javadoc.
@@ -8,11 +9,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class Calculator {
 
+  @Cacheable("sum")
   int sum(int a, int b) {
+    // sleep three seconds for testing purposes
+    try {
+      Thread.sleep(3000);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
     return a + b;
   }
 
+  @Cacheable("diff")
   int diff(int a, int b) {
+    // sleep three seconds for testing purposes
+    try {
+      Thread.sleep(3000);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
     return a - b;
   }
 
